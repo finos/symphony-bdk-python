317f0af5834a250e86cb181f3007e4f16b20af9d
PFSymphony <pf.vilquin@symphony.com>
2018-11-30 21:55:40 +0800

    added reading proxy username password and closed file
    added reading proxy username password and closed file

--- sym_api_client_python/configure/configure.py
+++ sym_api_client_python/configure/configure.py
@@ -30,6 +30,10 @@ class SymConfig():
             self.data['botEmailAddress'] = data['botEmailAddress']
             self.data['proxyURL'] = data['proxyURL']
             self.data['proxyPort'] = data['proxyPort']
+            self.data['proxyUsername'] = data['proxyUsername']
+            self.data['proxyPassword'] = data['proxyPassword']
+            
+            read_file.close()
 
 
     #load fields from confg.json into data dictionary on the SymConfig class (certificates)
@@ -47,6 +51,10 @@ class SymConfig():
             self.data['p.12'] = self.data['botCertPath'] + '.p12'
             self.data['proxyURL'] = data['proxyURL']
             self.data['proxyPort'] = data['proxyPort']
+            self.data['proxyUsername'] = data['proxyUsername']
+            self.data['proxyPassword'] = data['proxyPassword']
+            
+            read_file.close()  
 
         #take in .p12 certificate and parse through file to use for authentication
         #class returns symphonyCertificate and symphonyKey
