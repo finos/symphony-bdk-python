349760b924bbc97d562562f40c2c3bf572ea7d74
Reed Feldman <reed.feldman@symphony.com>
2020-02-10 12:07:56 -0500

    ready for review

--- sym_api_client_python/configure/configure.py
+++ sym_api_client_python/configure/configure.py
@@ -11,9 +11,9 @@ class SymConfig:
         given in this field, it should end with a slash.
 
         In testing one may want to set relative_to to be the path to the config json, so all references are relative
-        to that. An application may wish to set this to __file__ in its main, meaning an configuration file from anyway
+        to that. An application may wish to set this to __file__ in its main, meaning a configuration file from anywhere
         could be used.
-        
+
         """
         self.path_to_config = path_to_config
         self.relative_to = os.path.dirname(relative_to) if relative_to is not None else os.curdir
@@ -23,7 +23,7 @@ class SymConfig:
         """Given a json file, a key for a path and an optional key for a filename, extract the path
         and potentially name, resolve and join them. If warn_if_absent, issue a warning if the file
         or path does not exist at that location"""
-        
+
         path = json_data[path_key]
 
         # Blank values are used to ignore entry. They should probably be None instead of blank but to maintain
@@ -50,33 +50,33 @@ class SymConfig:
             data = json.load(read_file)
             self.data = data
 
-            if 'sessionAuthPort' in data:            
+            if 'sessionAuthPort' in data:
                 self.data['sessionAuthHost'] = 'https://'+ data['sessionAuthHost'] + ':' + str(data['sessionAuthPort'])
-            else: 
+            else:
                 self.data['sessionAuthHost'] = 'https://'+ data['sessionAuthHost']
 
             if 'keyAuthPort' in data:
                 self.data['keyAuthHost'] = 'https://'+ data['keyAuthHost'] + ':' + str(data['keyAuthPort'])
             else:
                 self.data['keyAuthHost'] = 'https://'+ data['keyAuthHost']
-            
+
             if 'podPort' in data:
                 self.data['podHost'] = 'https://'+ data['podHost'] + ':' + str(data['podPort'])
             else:
                 self.data['podHost'] = 'https://'+ data['podHost']
 
-            if 'agentPort' in data:        
+            if 'agentPort' in data:
                 self.data['agentHost'] = 'https://'+ data['agentHost'] + ':' + str(data['agentPort'])
             else:
                 self.data['agentHost'] = 'https://'+ data['agentHost']
-            
+
             #backwards compatible
             if 'botCertPath' in data:
                 self.data['botCertPath'] = self._fix_relative_path(data,'botCertPath')
 
             if 'botRSAName' in data:
                 self.data['botRSAPath'] = self._fix_relative_path(data, 'botRSAPath', 'botRSAName')
-            
+
             if 'botPrivateKeyName' in data:
                 # TODO: check this
                 self.data['botRSAPath'] = self._fix_relative_path(data, 'botPrivateKeyPath', 'botPrivateKeyName')
@@ -86,7 +86,7 @@ class SymConfig:
 
             if 'truststorePath' in data:
                 self.data['truststorePath'] = self._fix_relative_path(data, 'truststorePath')
-            
+
             if 'proxyURL' in data:
                 self.data['podProxyURL'] = data['proxyURL']
                 self.data['podProxyUsername'] = data['proxyUsername'] if 'proxyUsername' in data else ""
@@ -97,15 +97,15 @@ class SymConfig:
                 self.data['keyManagerProxyURL'] = data['proxyURL']
                 self.data['keyManagerProxyUsername'] = data['proxyUsername'] if 'proxyUsername' in data else ""
                 self.data['keyManagerProxyPassword'] = data['proxyPassword'] if 'proxyPassword' in data else ""
-        
-    
+
+
 
             if 'podProxyURL' not in data or not data['podProxyURL']:
                 self.data['podProxyRequestObject'] = {}
                 self.data['podProxyURL'] = ""
             else:
                 self.data['podProxyURL'] = data['podProxyURL']
-                
+
                 if 'podProxyUsername' in data and data['podProxyUsername']:
                     self.data['podProxyUsername'] = data['podProxyUsername']
                     self.data['podProxyPassword'] = data['podProxyPassword']
@@ -127,7 +127,7 @@ class SymConfig:
                 self.data['agentProxyURL'] = ""
             else:
                 self.data['agentProxyURL'] = data['agentProxyURL']
-                
+
                 if 'agentProxyUsername' in data and data['agentProxyUsername']:
                     self.data['agentProxyUsername'] = data['agentProxyUsername']
                     self.data['agentProxyPassword'] = data['agentProxyPassword']
@@ -149,7 +149,7 @@ class SymConfig:
                 self.data['keyManagerProxyURL'] = ""
             else:
                 self.data['keyManagerProxyURL'] = data['keyManagerProxyURL']
-                
+
                 if 'keyManagerProxyUsername' in data and data['keyManagerProxyUsername']:
                     self.data['keyManagerProxyUsername'] = data['keyManagerProxyUsername']
                     self.data['keyManagerProxyPassword'] = data['keyManagerProxyPassword']
@@ -165,7 +165,7 @@ class SymConfig:
                             'http' : data['keyManagerProxyURL'],
                             'https' : data['keyManagerProxyURL'],
                             }
-            
+
             if 'datafeedEventsErrorTimeout' in data:
                 self.data['datafeedEventsErrorTimeout'] = data['datafeedEventsErrorTimeout']
 
@@ -177,5 +177,3 @@ class SymConfig:
                     loggable_config_dict[k] = "---HIDDEN---"
 
             logging.info(json.dumps(loggable_config_dict, indent=4))
-
-
