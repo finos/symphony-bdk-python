4d9939af82e0a4979d31adc38967b904febab429
Elias Croze <elias.croze@symphony.com>
2020-08-19 16:17:27 +0200

    APP-2935: fixed relative paths in unit tests, made tests only use unittest

--- tests/clients/test_datafeed_client.py
+++ tests/clients/test_datafeed_client.py
@@ -1,51 +1,40 @@
-import pytest
+import os
+import unittest
 from unittest.mock import patch
 
-from sym_api_client_python.configure.configure import SymConfig
 from sym_api_client_python.clients.datafeed_client import DataFeedClient
 from sym_api_client_python.clients.datafeed_client_v1 import DataFeedClientV1
 from sym_api_client_python.clients.datafeed_client_v2 import DataFeedClientV2
+from sym_api_client_python.configure.configure import SymConfig
 
 
-@pytest.fixture
-def config():
-    configure = SymConfig('./tests/resources/bot-config.json', __file__)
-    configure.load_config()
-    return configure
+class TestDataFeedClient(unittest.TestCase):
+    def setUp(self):
+        self.config = SymConfig(get_path_relative_to_resources_folder('./bot-config.json'))
+        self.config.load_config()
+
+    @patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
+    def test_datafeed_client_v1(self, mock_client):
+        self.assert_configured_df_version_leads_to_df_client_type('v1', DataFeedClientV1)
+
+    @patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
+    def test_datafeed_client_v2(self, mock_client):
+        self.assert_configured_df_version_leads_to_df_client_type('v2', DataFeedClientV2)
+
+    @patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
+    def test_datafeed_client_wrong_version(self, mock_client):
+        self.assert_configured_df_version_leads_to_df_client_type('25', DataFeedClientV1)
+
+    def assert_configured_df_version_leads_to_df_client_type(self, datafeed_version, expected_class):
+        with patch('sym_api_client_python.clients.sym_bot_client.SymBotClient') as mock_client:
+            self.config.data['datafeedVersion'] = datafeed_version
+            mock_client.get_sym_config.return_value = self.config
+            datafeed_client = DataFeedClient(mock_client)
+
+            self.assertIsInstance(datafeed_client.datafeed_client, expected_class)
 
 
-@patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
-def test_datafeed_client_v1(mock_client, config):
-    config.data["datafeedVersion"] = "v1"
-
-    mock_client.get_sym_config.return_value = config
-
-    # Initialize datafeed client
-    datafeed_client = DataFeedClient(mock_client)
-
-    assert isinstance(datafeed_client.datafeed_client, DataFeedClientV1) == True
-
-@patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
-def test_datafeed_client_v2(mock_client, config):
-    config.data["datafeedVersion"] = "v2"
-
-    mock_client.get_sym_config.return_value = config
-
-    # Initialize datafeed client
-    datafeed_client = DataFeedClient(mock_client)
-    print(datafeed_client.datafeed_client)
-    assert isinstance(datafeed_client.datafeed_client, DataFeedClientV2) == True
-
-@patch('sym_api_client_python.clients.sym_bot_client.SymBotClient', autospec=True)
-def test_datafeed_client_wrong_version(mock_client, config):
-    config.data["datafeed_version"] = 25
-
-    mock_client.get_sym_config.return_value = config
-
-    # Initialize datafeed client
-    datafeed_client = DataFeedClient(mock_client)
-
-    assert isinstance(datafeed_client.datafeed_client, DataFeedClientV1) == True
-
-
+def get_path_relative_to_resources_folder(path_relative_to_resources):
+    path_to_resources = os.path.join(os.path.dirname(__file__), '../resources/', path_relative_to_resources)
+    return os.path.normpath(path_to_resources)
 
