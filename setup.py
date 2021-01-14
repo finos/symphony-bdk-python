b40ddcbba4642cebe7f57a5825c04c48cf8d45c8
Youri Bonnaffé <63661676+symphony-youri@users.noreply.github.com>
2021-01-14 09:13:08 +0100

    PLAT-10373: Bump version to 1.3.2 (#97)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setuptools.setup(
     name="sym_api_client_python",
 
-    version="1.3.1",
+    version="1.3.2",
     author="Symphony Platform Solutions",
     author_email="platformsolutions@symphony.com",
     description="Symphony REST API - Python Client",
