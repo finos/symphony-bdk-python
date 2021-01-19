70f0abceb9b8c13a161d5badf5545578b8693541
Youness El idrissi <youness.idrissi@symphony.com>
2021-01-19 16:34:05 +0100

    changed constructor params

--- symphony/bdk/core/config/model/bdk_ssl_config.py
+++ symphony/bdk/core/config/model/bdk_ssl_config.py
@@ -4,9 +4,11 @@ from symphony.bdk.core.config.model.bdk_certificate_config import BdkCertificate
 class BdkSslConfig:
     """Class containing the ssl configuration
     """
+
     def __init__(self, config):
-        if config is not None:
-            self.trust_store = BdkCertificateConfig(
-                **config.get("trustStore")) if "trustStore" in config else BdkCertificateConfig()
+        if config is not None and "trustStore" in config:
+            truststore_config = config.get("trustStore")
+            self.trust_store = BdkCertificateConfig(path=truststore_config.get("path"),
+                                                    password=truststore_config.get("password"))
         else:
             self.trust_store = BdkCertificateConfig()
