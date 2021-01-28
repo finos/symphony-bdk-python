b0e1e3bd2744fefa86270eb96b4d00dce2494984
symphony-hong <hong.le@symphony.com>
2021-01-28 12:01:39 +0100

    Message Service implementation.
    Create MultiAttachmentsMessageApi class inherited MessagesApi allow bot to call the api which will send a message with multiple attachments and files as a list of files.
    
    Create MessageService class wrapping all the service api related to message service which are documented in the https://developers.symphony.com/restapi/reference#messages-v4
    
    Create method in the SymphonyBdk class to initialize the MessageService instance and provide it to bot developer to work with message service.
    
    Create an example which shows how a bot interact with the message service.
    
    Fix wrong path for agent api client.

--- symphony/bdk/core/symphony_bdk.py
+++ symphony/bdk/core/symphony_bdk.py
@@ -1,6 +1,14 @@
-from symphony.bdk.core.client.api_client_factory import ApiClientFactory
-from symphony.bdk.core.auth.authenticator_factory import AuthenticatorFactory
 from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.auth.authenticator_factory import AuthenticatorFactory
+from symphony.bdk.core.client.api_client_factory import ApiClientFactory
+from symphony.bdk.core.service.message_service import MessageService
+from symphony.bdk.core.service.message_service import MultiAttachmentsMessagesApi
+from symphony.bdk.gen.agent_api.attachments_api import AttachmentsApi
+from symphony.bdk.gen.pod_api.default_api import DefaultApi
+from symphony.bdk.gen.pod_api.message_api import MessageApi
+from symphony.bdk.gen.pod_api.message_suppression_api import MessageSuppressionApi
+from symphony.bdk.gen.pod_api.pod_api import PodApi
+from symphony.bdk.gen.pod_api.streams_api import StreamsApi
 
 
 class SymphonyBdk:
@@ -11,9 +19,12 @@ class SymphonyBdk:
     def __init__(self, config):
         self._config = config
         self._api_client_factory = ApiClientFactory(config)
+        self._pod_client = self._api_client_factory.get_pod_client()
+        self._agent_client = self._api_client_factory.get_agent_client()
 
         self._authenticator_factory = AuthenticatorFactory(config, api_client_factory=self._api_client_factory)
         self._bot_session = None
+        self._message_service = None
 
     async def bot_session(self) -> AuthSession:
         """
@@ -25,6 +36,20 @@ class SymphonyBdk:
         if self._bot_session is None:
             self._bot_session = await self._authenticator_factory.get_bot_authenticator().authenticate_bot()
         return self._bot_session
+
+    async def messages(self):
+        if self._message_service is None:
+            self._message_service = MessageService(
+                MultiAttachmentsMessagesApi(self._agent_client),
+                MessageApi(self._pod_client),
+                MessageSuppressionApi(self._pod_client),
+                StreamsApi(self._pod_client),
+                PodApi(self._pod_client),
+                AttachmentsApi(self._agent_client),
+                DefaultApi(self._pod_client),
+                await self.bot_session()
+            )
+        return self._message_service
 
     async def close_clients(self):
         """
