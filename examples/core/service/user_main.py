587f788aedf079c5196cb49d2cc352089b41826d
symphony-hong <hong.le@symphony.com>
2021-02-07 00:14:21 +0100

    Update service example

--- examples/core/service/user_main.py
+++ examples/core/service/user_main.py
@@ -9,9 +9,9 @@ class UserMain:
     @staticmethod
     async def run():
 
-        config_3 = BdkConfigLoader.load_from_symphony_dir("config.yaml")
+        config = BdkConfigLoader.load_from_symphony_dir("config.yaml")
 
-        async with SymphonyBdk(config_3) as bdk:
+        async with SymphonyBdk(config) as bdk:
             user_service = bdk.users()
             details = await user_service.list_users_by_ids([12987981103610])
             print(details)
