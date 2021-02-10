ba186c4e799225d0626c595b0e1ff65dd8d5931f
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2021-02-10 02:24:26 -0800

    PLAT-10370: Connection Service Implementation (#118)
    * Connection Service Implementation
    
    ConnectionService class in connection_service.py to wrap all the connection endpoints presented in https://developers.symphony.com/restapi/reference
    
    ConnectionStatus enumeration in connection_status.py listing out all the available value can be set to Connection status.
    
    ServiceFactory class in service_factory.py to construct the Service and provide them to the SymphonyBdk entry point.
    
    SymphonyBdk#connections to use the connection service from the Bdk entry point.
    
    ConnectionService example added.
    
    Unittest added.
    
    Docstring added.
    
    * Using ConnectionService#list_connections in example to avoid hardcoding
    
    * Using ConnectionStatus.ALL instead of None for default value
    
    * Update documentation
    
    * Update example

---
+++ symphony/bdk/core/service/connection/connection_service.py
@@ -0,0 +1,133 @@
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.service.connection.model.connection_status import ConnectionStatus
+from symphony.bdk.gen.pod_api.connection_api import ConnectionApi
+from symphony.bdk.gen.pod_model.user_connection import UserConnection
+from symphony.bdk.gen.pod_model.user_connection_request import UserConnectionRequest
+
+
+class ConnectionService:
+    """Service class for managing the connections between users
+
+    This service is used for retrieving the connection status if the calling user with a specified user or with many
+    other internal or external users in the pod, and perform some actions related to the connection status like:
+
+    * Send a connection request to an user
+    * Accept a connection request from an user
+    * Reject a connection request from an user
+    * Remove a connection with an user
+    """
+
+    def __init__(self, connection_api: ConnectionApi, auth_session: AuthSession):
+        self._connection_api = connection_api
+        self._auth_session = auth_session
+
+    async def get_connection(self, user_id: int) -> UserConnection:
+        """
+        Get connection status, i.e. check if the calling user is connected to the specified user.
+        See: `Get Connection <https://developers.symphony.com/restapi/reference#get-connection>`_
+
+        :param user_id: The id of the user with whom the caller want to check.
+
+        :return: Connection status with the specified user.
+
+        """
+        params = {
+            'user_id': str(user_id),
+            'session_token': await self._auth_session.session_token
+        }
+        return await self._connection_api.v1_connection_user_user_id_info_get(**params)
+
+    async def list_connections(
+            self,
+            status: ConnectionStatus = ConnectionStatus.ALL,
+            user_ids: [int] = None
+    ) -> [UserConnection]:
+        """
+        List all connection statuses of the requesting user with external or specified users.
+        See: `List Connections <https://developers.symphony.com/restapi/reference#list-connections>`_
+
+        :param status:      Filter the connection list based on the connection status.
+                            The connection status can only be pending_incoming, pending_outgoing,
+                            accepted, rejected, or all.
+                            If you do not specify a status, all connections will be returned.
+        :param user_ids:    List of user ids which are used to restrict the list of results.
+                            This can be used to return connections with internal users;
+                            although, by default, this endpoint does not list implicit connections with internal users.
+
+        :return: List of connection statuses with the specified users and status.
+
+        """
+        params = {
+            'status': status.value,
+            'session_token': await self._auth_session.session_token
+        }
+        if user_ids is not None:
+            params['user_ids'] = ','.join(map(str, user_ids))
+
+        user_connection_list = await self._connection_api.v1_connection_list_get(**params)
+        return user_connection_list.value
+
+    async def create_connection(self, user_id: int) -> UserConnection:
+        """
+        Sends a connection request to another user.
+        See: `Create Connection <https://developers.symphony.com/restapi/reference#create-connection>`_
+
+        :param user_id: The id of the user with whom the caller want to connect.
+
+        :return: Connection status with the specified user.
+
+        """
+        user_connection_request = UserConnectionRequest(user_id=user_id)
+        params = {
+            'connection_request': user_connection_request,
+            'session_token': await self._auth_session.session_token
+        }
+        return await self._connection_api.v1_connection_create_post(**params)
+
+    async def accept_connection(self, user_id: int) -> UserConnection:
+        """
+        Accept the connection request from a requesting user.
+        See: `Accept Connection <https://developers.symphony.com/restapi/reference#accepted-connection>`_
+
+        :param user_id: The id of the user who requested to connect with the caller.
+
+        :return: Connection status with the requesting user.
+
+        """
+        user_connection_request = UserConnectionRequest(user_id=user_id)
+        params = {
+            'connection_request': user_connection_request,
+            'session_token': await self._auth_session.session_token
+        }
+        return await self._connection_api.v1_connection_accept_post(**params)
+
+    async def reject_connection(self, user_id: int) -> UserConnection:
+        """
+        Reject the connection request from a requesting user.
+        See: `Reject Connection <https://developers.symphony.com/restapi/reference#reject-connection>`_
+
+        :param user_id: The id of the user who requested to connect with the caller.
+
+        :return: Connection status with the requesting user.
+
+        """
+        user_connection_request = UserConnectionRequest(user_id=user_id)
+        params = {
+            'connection_request': user_connection_request,
+            'session_token': await self._auth_session.session_token
+        }
+        return await self._connection_api.v1_connection_reject_post(**params)
+
+    async def remove_connection(self, user_id: int) -> None:
+        """
+        Removes a connection with a user.
+        See: `Remove Connection <https://developers.symphony.com/restapi/reference#remove-connection>`_
+
+        :param user_id: The id of the user with whom we want to remove the connection.
+
+        """
+        params = {
+            'uid': user_id,
+            'session_token': await self._auth_session.session_token
+        }
+        await self._connection_api.v1_connection_user_uid_remove_post(**params)
---
+++ symphony/bdk/core/service_factory.py
@@ -0,0 +1,96 @@
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.client.api_client_factory import ApiClientFactory
+from symphony.bdk.core.config.model.bdk_config import BdkConfig
+from symphony.bdk.core.service.connection.connection_service import ConnectionService
+from symphony.bdk.core.service.datafeed.datafeed_loop_v1 import DatafeedLoopV1
+from symphony.bdk.core.service.message.message_service import MessageService
+from symphony.bdk.core.service.message.multi_attachments_messages_api import MultiAttachmentsMessagesApi
+from symphony.bdk.core.service.user.user_service import UserService
+from symphony.bdk.gen.agent_api.attachments_api import AttachmentsApi
+from symphony.bdk.gen.agent_api.audit_trail_api import AuditTrailApi
+from symphony.bdk.gen.agent_api.datafeed_api import DatafeedApi
+from symphony.bdk.gen.pod_api.connection_api import ConnectionApi
+from symphony.bdk.gen.pod_api.default_api import DefaultApi
+from symphony.bdk.gen.pod_api.message_api import MessageApi
+from symphony.bdk.gen.pod_api.message_suppression_api import MessageSuppressionApi
+from symphony.bdk.gen.pod_api.pod_api import PodApi
+from symphony.bdk.gen.pod_api.streams_api import StreamsApi
+from symphony.bdk.gen.pod_api.system_api import SystemApi
+from symphony.bdk.gen.pod_api.user_api import UserApi
+from symphony.bdk.gen.pod_api.users_api import UsersApi
+
+
+class ServiceFactory:
+    """
+    Factory responsible for creating BDK service instances for Symphony Bdk entry point:
+    * User Service
+    * Message Service
+    * Connection Service
+    * Datafeed Loop
+    * ...
+    """
+
+    def __init__(
+            self,
+            api_client_factory: ApiClientFactory,
+            auth_session: AuthSession,
+            config: BdkConfig
+    ):
+        self._pod_client = api_client_factory.get_pod_client()
+        self._agent_client = api_client_factory.get_agent_client()
+        self._auth_session = auth_session
+        self._config = config
+
+    def get_user_service(self) -> UserService:
+        """
+        Returns a fully initialized UserService
+
+        :return: a new UserService instance.
+        """
+        return UserService(
+            UserApi(self._pod_client),
+            UsersApi(self._pod_client),
+            AuditTrailApi(self._agent_client),
+            SystemApi(self._pod_client),
+            self._auth_session
+        )
+
+    def get_message_service(self) -> MessageService:
+        """
+        Returns a fully initialized MessageService
+
+        :return: a new MessageService instance.
+        """
+        return MessageService(
+            MultiAttachmentsMessagesApi(self._agent_client),
+            MessageApi(self._pod_client),
+            MessageSuppressionApi(self._pod_client),
+            StreamsApi(self._pod_client),
+            PodApi(self._pod_client),
+            AttachmentsApi(self._agent_client),
+            DefaultApi(self._pod_client),
+            self._auth_session
+        )
+
+    def get_connection_service(self) -> ConnectionService:
+        """
+        Returns a fully initialized ConnectionService
+
+        :return: a new ConnectionService instance.
+        """
+        return ConnectionService(
+            ConnectionApi(self._pod_client),
+            self._auth_session
+        )
+
+    def get_datafeed_loop(self) -> DatafeedLoopV1:
+        """
+        Returns a fully initialized DatafeedLoop
+
+        :return: a new DatafeedLoop instance.
+        """
+        return DatafeedLoopV1(
+            DatafeedApi(self._agent_client),
+            self._auth_session,
+            self._config
+        )
