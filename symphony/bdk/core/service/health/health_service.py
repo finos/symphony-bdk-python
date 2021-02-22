7d672d90c573c485e61174469ad59d8dcf867205
Youness El idrissi <youness.idrissi@symphony.com>
2021-02-22 14:51:00 +0100

    PLAT-10483 Implementation of the health service - implementation - tests

---
+++ symphony/bdk/core/service/health/health_service.py
@@ -0,0 +1,42 @@
+from symphony.bdk.gen.agent_api.system_api import SystemApi
+from symphony.bdk.gen.agent_api.signals_api import SignalsApi
+
+from symphony.bdk.gen.agent_model.v3_health import V3Health
+
+
+class HealthService:
+
+    def __init__(self, system_api: SystemApi, signals_api: SignalsApi):
+        self.system_api = system_api
+        self.signals_api = signals_api
+
+    async def health_check(self) -> V3Health:
+        """ Returns the connectivity status of your Agent server.
+        If your Agent server is started and running properly, the status value will be UP.
+        Available on Agent 2.57.0 and above.
+
+        :return: V3Health: the connectivity status of your Agent server.
+        `Health Check v3 <https://developers.symphony.com/restapi/reference#health-check-v3>`_
+        """
+        response = await self.system_api.v3_health()
+        return response
+
+    async def health_check_extended(self) -> V3Health:
+        """Returns the connectivity status of the Agent services as well as users connectivity.
+        Available on Agent 2.57.0 and above.
+
+        :return: V3Health: the connectivity status of the Agent services as well as users connectivity.
+        `Healt Check Extended v3 <https://developers.symphony.com/restapi/reference#health-check-extended-v3>`_
+        """
+        response = await self.system_api.v3_extended_health()
+        return response
+
+    async def get_agent_info(self):
+        """Gets information about the Agent.
+        Available on Agent 2.53.0 and above.
+
+        :return: AgentInfo: information of  the agent server.
+        `Agent Info v1 <https://developers.symphony.com/restapi/reference#agent-info-v1>`_
+        """
+        response = await self.signals_api.v1_info_get()
+        return response
---
+++ tests/core/service/health/test_health_service.py
@@ -0,0 +1,100 @@
+import pytest
+from unittest.mock import MagicMock, AsyncMock
+
+from symphony.bdk.gen import ApiException
+from symphony.bdk.gen.agent_model.agent_info import AgentInfo
+from tests.utils.resource_utils import object_from_json_relative_path
+
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.service.health.health_service import HealthService
+
+from symphony.bdk.gen.agent_api.system_api import SystemApi
+from symphony.bdk.gen.agent_api.signals_api import SignalsApi
+from symphony.bdk.gen.agent_model.v3_health import V3Health
+
+
+@pytest.fixture()
+def auth_session():
+    auth_session = AuthSession(None)
+    auth_session.session_token = "session_token"
+    auth_session.key_manager_token = "km_token"
+    return auth_session
+
+
+@pytest.fixture()
+def mocked_system_api_client():
+    api_client = MagicMock(SystemApi)
+    api_client.v3_health = AsyncMock()
+    api_client.v3_extended_health = AsyncMock()
+    return api_client
+
+
+@pytest.fixture()
+def mocked_signals_api_client():
+    api_client = MagicMock(SignalsApi)
+    api_client.v1_info_get = AsyncMock()
+    return api_client
+
+
+@pytest.fixture()
+def health_service(mocked_system_api_client, mocked_signals_api_client):
+    return HealthService(mocked_system_api_client, mocked_signals_api_client)
+
+
+@pytest.mark.asyncio
+async def test_health_check(health_service, mocked_system_api_client):
+    mocked_system_api_client.v3_health.return_value = object_from_json_relative_path(
+        "health_response/health_check.json")
+
+    health: V3Health = await health_service.health_check()
+
+    assert health.status == "UP"
+
+
+@pytest.mark.asyncio
+async def test_health_check_failed(health_service, mocked_system_api_client):
+    mocked_system_api_client.v3_health.side_effect = ApiException(
+        400)  # TODO: Check if we should add an ApiRuntimeException
+
+    with pytest.raises(ApiException):
+        await health_service.health_check()
+
+
+@pytest.mark.asyncio
+async def test_health_check_extended(health_service, mocked_system_api_client):
+    mocked_system_api_client.v3_extended_health.return_value = object_from_json_relative_path(
+        "health_response/health_check_extended.json")
+    health: V3Health = await health_service.health_check_extended()
+
+    assert health.status == "UP"
+    assert health.services.pod.version == "1.57.0"
+    assert health.services.datafeed.status == "UP"
+    assert health.users.agentservice.status == "UP"
+
+
+@pytest.mark.asyncio
+async def test_health_check_extended_failed(health_service, mocked_system_api_client):
+    mocked_system_api_client.v3_extended_health.side_effect = ApiException(400)
+
+    with pytest.raises(ApiException):
+        await health_service.health_check_extended()
+
+
+@pytest.mark.asyncio
+async def test_get_agent_info(health_service, mocked_signals_api_client):
+    mocked_signals_api_client.v1_info_get.return_value = object_from_json_relative_path(
+        "health_response/agent_info.json")
+
+    agent_info: AgentInfo = await health_service.get_agent_info()
+
+    assert agent_info.hostname == "agent-75...4b6"
+    assert agent_info.ipAddress == "22.222.222.22"
+    assert agent_info.onPrem == True
+
+
+@pytest.mark.asyncio
+async def test_get_agent_info_failed(health_service, mocked_signals_api_client):
+    mocked_signals_api_client.v1_info_get.side_effect = ApiException(400)
+
+    with pytest.raises(ApiException):
+        await health_service.get_agent_info()
