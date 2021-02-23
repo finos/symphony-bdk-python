a0bd361d68a065436a18f0787c99b9d033c201ca
Youness El idrissi <youness.idrissi@symphony.com>
2021-02-23 16:04:26 +0100

    PLAT-10483 Modified docstrings

--- symphony/bdk/core/service/health/health_service.py
+++ symphony/bdk/core/service/health/health_service.py
@@ -13,28 +13,28 @@ class HealthService:
 
     async def health_check(self) -> V3Health:
         """ Returns the connectivity status of your Agent server.
+        Wraps the `Health Check v3 <https://developers.symphony.com/restapi/reference#health-check-v3>`_ endpoint.
         If your Agent server is started and running properly, the status value will be UP.
         Available on Agent 2.57.0 and above.
 
         :return: V3Health: the connectivity status of your Agent server.
-        `Health Check v3 <https://developers.symphony.com/restapi/reference#health-check-v3>`_
         """
         return await self._system_api.v3_health()
 
     async def health_check_extended(self) -> V3Health:
         """Returns the connectivity status of the Agent services as well as users connectivity.
+        Wraps the `Healt Check Extended v3 <https://developers.symphony.com/restapi/reference#health-check-extended-v3>`_ endpoint.
         Available on Agent 2.57.0 and above.
 
         :return: V3Health: the connectivity status of the Agent services as well as users connectivity.
-        `Healt Check Extended v3 <https://developers.symphony.com/restapi/reference#health-check-extended-v3>`_
         """
         return await self._system_api.v3_extended_health()
 
     async def get_agent_info(self):
         """Gets information about the Agent.
+        Wraps the `Agent Info v1 <https://developers.symphony.com/restapi/reference#agent-info-v1>`_ endpoint.
         Available on Agent 2.53.0 and above.
 
         :return: AgentInfo: information of  the agent server.
-        `Agent Info v1 <https://developers.symphony.com/restapi/reference#agent-info-v1>`_
         """
         return await self._signals_api.v1_info_get()
