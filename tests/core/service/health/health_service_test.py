b39e4b47c102d04f5ba2b048850eea3e935cc803
Youness El idrissi <youness.idrissi@symphony.com>
2021-02-23 10:30:02 +0100

    PLAT-10483 Removed type hints from tests

--- tests/core/service/health/health_service_test.py
+++ tests/core/service/health/health_service_test.py
@@ -2,14 +2,12 @@ from unittest.mock import MagicMock, AsyncMock
 import pytest
 
 from symphony.bdk.gen import ApiException
-from symphony.bdk.gen.agent_model.agent_info import AgentInfo
 from tests.utils.resource_utils import object_from_json_relative_path
 
 from symphony.bdk.core.service.health.health_service import HealthService
 
 from symphony.bdk.gen.agent_api.system_api import SystemApi
 from symphony.bdk.gen.agent_api.signals_api import SignalsApi
-from symphony.bdk.gen.agent_model.v3_health import V3Health
 
 
 @pytest.fixture()
@@ -37,7 +35,7 @@ async def test_health_check(health_service, mocked_system_api_client):
     mocked_system_api_client.v3_health.return_value = object_from_json_relative_path(
         "health_response/health_check.json")
 
-    health: V3Health = await health_service.health_check()
+    health = await health_service.health_check()
 
     assert health.status == "UP"
 
@@ -54,7 +52,7 @@ async def test_health_check_failed(health_service, mocked_system_api_client):
 async def test_health_check_extended(health_service, mocked_system_api_client):
     mocked_system_api_client.v3_extended_health.return_value = object_from_json_relative_path(
         "health_response/health_check_extended.json")
-    health: V3Health = await health_service.health_check_extended()
+    health = await health_service.health_check_extended()
 
     assert health.status == "UP"
     assert health.services.pod.version == "1.57.0"
@@ -75,7 +73,7 @@ async def test_get_agent_info(health_service, mocked_signals_api_client):
     mocked_signals_api_client.v1_info_get.return_value = object_from_json_relative_path(
         "health_response/agent_info.json")
 
-    agent_info: AgentInfo = await health_service.get_agent_info()
+    agent_info = await health_service.get_agent_info()
 
     assert agent_info.hostname == "agent-75...4b6"
     assert agent_info.ipAddress == "22.222.222.22"
