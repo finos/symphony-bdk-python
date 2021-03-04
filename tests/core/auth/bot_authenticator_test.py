83245a544826bdb4c5837f901400e7a9dd95404d
Soufiane Aourinmouche <52406574+symphony-soufiane@users.noreply.github.com>
2021-03-04 18:41:22 +0100

    PLAT-10529 Ability to set private key and certificate content programmatically (#137)
    Private key and Certificate were able to only be loaded from the config file when initializing bdk. In this PR, we update bot config to be able to set private key and certificate after bdk being initialized.

--- tests/core/auth/bot_authenticator_test.py
+++ tests/core/auth/bot_authenticator_test.py
@@ -2,6 +2,7 @@ from unittest.mock import patch, MagicMock, AsyncMock
 
 import pytest
 
+from tests.utils.resource_utils import get_resource_filepath
 from symphony.bdk.core.auth.bot_authenticator import BotAuthenticatorRsa
 from symphony.bdk.core.auth.exception import AuthUnauthorizedError
 from symphony.bdk.core.config.model.bdk_config import *
@@ -9,7 +10,6 @@ from symphony.bdk.gen.api_client import ApiClient
 from symphony.bdk.gen.configuration import Configuration
 from symphony.bdk.gen.exceptions import ApiException
 from symphony.bdk.gen.login_model.token import Token
-
 
 @pytest.fixture()
 def config():
@@ -21,7 +21,6 @@ def config():
     }
     return BdkBotConfig(bot_config)
 
-
 @pytest.fixture()
 def mocked_api_client():
     def __loader():
@@ -31,7 +30,6 @@ def mocked_api_client():
         return api_client
 
     return __loader
-
 
 @pytest.mark.asyncio
 async def test_bot_session(config, mocked_api_client):
@@ -80,5 +78,27 @@ async def test_authenticate_bot(config, mocked_api_client):
         bot_authenticator = BotAuthenticatorRsa(config, login_api_client, relay_api_client)
         auth_session = await bot_authenticator.authenticate_bot()
 
+        assert await auth_session.session_token == "session_token"
+        assert await auth_session.key_manager_token == "km_token"
+
+@pytest.mark.asyncio
+async def test_authenticate_with_private_key_content(config, mocked_api_client):
+    with patch('symphony.bdk.core.auth.bot_authenticator.create_signed_jwt', return_value='privateKey'):
+        login_api_client = mocked_api_client()
+        relay_api_client = mocked_api_client()
+
+        login_api_client.call_api.return_value = Token(token="session_token")
+        relay_api_client.call_api.return_value = Token(token="km_token")
+
+        private_key_string = get_resource_filepath('key/private_key.pem', as_text=False).read_text()
+        assert config.private_key._path is not None
+        assert config.private_key._content == ""
+        config.private_key.content = private_key_string
+
+        bot_authenticator = BotAuthenticatorRsa(config, login_api_client, relay_api_client)
+        auth_session = await bot_authenticator.authenticate_bot()
+
+        assert config.private_key._path is None
+        assert config.private_key._content == private_key_string
         assert await auth_session.session_token == "session_token"
         assert await auth_session.key_manager_token == "km_token"
