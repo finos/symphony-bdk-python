5b2249730d6e9dad293fde78cee16ecb3764abcb
Youri Bonnaffe <youri.bonnaffe@symphony.com>
2021-03-08 14:37:56 +0100

    PLAT-10643: Unit test system certs loading

---
+++ tests/bdk/gen/rest_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from symphony.bdk.gen import rest, Configuration
+
+
+@pytest.mark.asyncio
+async def test_system_certs_are_loaded():
+    rest_client = rest.RESTClientObject(Configuration())
+
+    # Calling an URL with a valid HTTPS cert
+    response = await rest_client.GET("https://google.fr")
+
+    assert response.status == 200
