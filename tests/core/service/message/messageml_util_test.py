4645247e5e335c5b3c38379e85ba6a2a8f6a7d9d
Mariacristina De Dominicis <65179248+symphony-mariacristina@users.noreply.github.com>
2021-03-17 10:39:13 +0100

    PLAT-10565 Adding utils classes (#150)
    Adding some util classes on message processing to:
    
    Extract entities from a given incoming message (mentions, hashtags, cashtags, emojis)
    Extract message content from presentationML
    Pre-process an outgoing message by "cleaning" the text message, escaping all special characters that will violate messageML format
    Convert a stream id into a url safe id and viceversa

---
+++ tests/core/service/message/messageml_util_test.py
@@ -0,0 +1,24 @@
+from symphony.bdk.core.service.message.messageml_util import escape_special_chars
+
+
+def test_escape_special_chars_match_found():
+    text = "\\.hello"
+    expected_text = "&#92;&#46;hello"
+    assert expected_text == escape_special_chars(text)
+
+
+def test_escape_special_chars_no_match_found():
+    text = "hello."
+    expected_text = "hello&#46;"
+    assert expected_text == escape_special_chars(text)
+
+
+def test_escape_special_chars_multiple_matches():
+    text = "Here's multiple chars <'\"$#=.[]"
+    expected_text = "Here&apos;s multiple chars &lt;&apos;&quot;&#36;&#35;&#61;&#46;&#91;&#93;"
+    assert expected_text == escape_special_chars(text)
+
+
+def test_escape_special_chars_no_change():
+    text = "  This text will remain the same  "
+    assert text == escape_special_chars(text)
