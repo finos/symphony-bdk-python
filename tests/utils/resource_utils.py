8c92c19bcf1c01f9996744e989c0b3c3cd08a5d3
Youness El idrissi <youness.idrissi@symphony.com>
2021-04-08 12:38:22 +0200

    PLAT-10555 Deserialize using validate_and_convert_types

--- tests/utils/resource_utils.py
+++ tests/utils/resource_utils.py
@@ -1,10 +1,8 @@
-from collections import namedtuple
 from pathlib import Path
 import json
 from types import SimpleNamespace
 
-from symphony.bdk.gen import ApiClient
-from symphony.bdk.gen.rest import RESTResponse
+from symphony.bdk.gen.api_client import validate_and_convert_types
 from symphony.bdk.gen.configuration import Configuration
 
 
@@ -41,9 +39,16 @@ def deserialize_object(model, payload):
     :param payload: json payload to be deserialized
     :return: Instance of the model
     """
-    response = namedtuple("MockResp", ["status", "reason"])(200, "")
-    response = RESTResponse(response, payload)
-    return ApiClient(configuration=Configuration(discard_unknown_keys=True)).deserialize(response, (model,), True)
+    try:
+        test_data = json.loads(payload)
+    except json.JSONDecodeError:
+        test_data = payload
+    return validate_and_convert_types(input_value=test_data,
+                                      required_types_mixed=(model,),
+                                      path_to_item=["test_data"],
+                                      spec_property_naming=True,
+                                      _check_type=True,
+                                      configuration=Configuration(discard_unknown_keys=True))
 
 
 def get_config_resource_filepath(relative_path):
