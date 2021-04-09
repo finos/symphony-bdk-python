fa64a0525848054cd59a5a24d597b4baa33bf2e2
symphony-youness <76746033+symphony-youness@users.noreply.github.com>
2021-04-09 15:13:59 +0200

    PLAT-10555: Deserializing models in tests (#168)
    Update the tests to use the generated models instead of creating objects from a JSON file.

--- tests/core/service/connection/connection_service_test.py
+++ tests/core/service/connection/connection_service_test.py
@@ -6,8 +6,10 @@ from symphony.bdk.core.auth.auth_session import AuthSession
 from symphony.bdk.core.service.connection.connection_service import ConnectionService
 from symphony.bdk.core.service.connection.model.connection_status import ConnectionStatus
 from symphony.bdk.gen.pod_api.connection_api import ConnectionApi
+from symphony.bdk.gen.pod_model.user_connection import UserConnection
+from symphony.bdk.gen.pod_model.user_connection_list import UserConnectionList
 from symphony.bdk.gen.pod_model.user_connection_request import UserConnectionRequest
-from tests.utils.resource_utils import object_from_json
+from tests.utils.resource_utils import object_from_json, deserialize_object
 
 
 @pytest.fixture(name="auth_session")
@@ -35,12 +37,11 @@ def fixture_connection_service(connection_api, auth_session):
 @pytest.mark.asyncio
 async def test_get_connection(connection_api, connection_service):
     connection_api.v1_connection_user_user_id_info_get = AsyncMock()
-    connection_api.v1_connection_user_user_id_info_get.return_value = object_from_json(
-        "{"
-        "   \"userId\": 769658112378,"
-        "   \"status\": \"ACCEPTED\""
-        "}"
-    )
+    connection_api.v1_connection_user_user_id_info_get.return_value = \
+        deserialize_object(UserConnection, "{"
+                                           "   \"userId\": 769658112378,"
+                                           "   \"status\": \"ACCEPTED\""
+                                           "}")
 
     user_connection = await connection_service.get_connection(769658112378)
 
@@ -48,29 +49,26 @@ async def test_get_connection(connection_api, connection_service):
         user_id="769658112378",
         session_token="session_token"
     )
-    assert user_connection.userId == 769658112378
+    assert user_connection.user_id == 769658112378
     assert user_connection.status == ConnectionStatus.ACCEPTED.value
 
 
 @pytest.mark.asyncio
 async def test_list_connections(connection_api, connection_service):
     connection_api.v1_connection_list_get = AsyncMock()
-    connection_api.v1_connection_list_get.return_value = object_from_json(
-        "{"
-        "\"value\": ["
-        "   {"
-        "       \"userId\": 7078106126503,"
-        "       \"status\": \"PENDING_OUTGOING\","
-        "       \"updatedAt\": 1471018076255"
-        "   },"
-        "   {"
-        "       \"userId\": 7078106103809,"
-        "       \"status\": \"PENDING_INCOMING\","
-        "       \"updatedAt\": 1467562406219"
-        "   }"
-        "]"
-        "}"
-    )
+    connection_api.v1_connection_list_get.return_value = deserialize_object(UserConnectionList,
+                                                                            "["
+                                                                            "   {"
+                                                                            "       \"userId\": 7078106126503,"
+                                                                            "       \"status\": \"PENDING_OUTGOING\","
+                                                                            "       \"updatedAt\": 1471018076255"
+                                                                            "   },"
+                                                                            "   {"
+                                                                            "       \"userId\": 7078106103809,"
+                                                                            "       \"status\": \"PENDING_INCOMING\","
+                                                                            "       \"updatedAt\": 1467562406219"
+                                                                            "   }"
+                                                                            "]")
 
     user_connections = await connection_service.list_connections(ConnectionStatus.ALL, [7078106126503, 7078106103809])
 
@@ -81,22 +79,21 @@ async def test_list_connections(connection_api, connection_service):
     )
 
     assert len(user_connections) == 2
-    assert user_connections[0].userId == 7078106126503
+    assert user_connections[0].user_id == 7078106126503
     assert user_connections[1].status == ConnectionStatus.PENDING_INCOMING.value
 
 
 @pytest.mark.asyncio
 async def test_create_connection(connection_api, connection_service):
     connection_api.v1_connection_create_post = AsyncMock()
-    connection_api.v1_connection_create_post.return_value = object_from_json(
-        "{"
-        "  \"userId\": 7078106126503,"
-        "  \"status\": \"PENDING_OUTGOING\","
-        "  \"firstRequestedAt\": 1471018076255,"
-        "  \"updatedAt\": 1471018076255,"
-        "  \"requestCounter\": 1"
-        "}"
-    )
+    connection_api.v1_connection_create_post.return_value = deserialize_object(UserConnection,
+                                                                               "{"
+                                                                               "  \"userId\": 7078106126503,"
+                                                                               "  \"status\": \"PENDING_OUTGOING\","
+                                                                               "  \"firstRequestedAt\": 1471018076255,"
+                                                                               "  \"updatedAt\": 1471018076255,"
+                                                                               "  \"requestCounter\": 1"
+                                                                               "}")
 
     user_connection = await connection_service.create_connection(7078106126503)
 
@@ -105,23 +102,22 @@ async def test_create_connection(connection_api, connection_service):
         session_token="session_token"
     )
 
-    assert user_connection.userId == 7078106126503
-    assert user_connection.requestCounter == 1
+    assert user_connection.user_id == 7078106126503
+    assert user_connection.request_counter == 1
     assert user_connection.status == ConnectionStatus.PENDING_OUTGOING.value
 
 
 @pytest.mark.asyncio
 async def test_accept_connection(connection_api, connection_service):
     connection_api.v1_connection_accept_post = AsyncMock()
-    connection_api.v1_connection_accept_post.return_value = object_from_json(
-        "{"
-        "   \"userId\": 7078106169577,"
-        "   \"status\": \"ACCEPTED\","
-        "   \"firstRequestedAt\": 1471046357339,"
-        "   \"updatedAt\": 1471046517684,"
-        "   \"requestCounter\": 1"
-        "}"
-    )
+    connection_api.v1_connection_accept_post.return_value = deserialize_object(UserConnection,
+                                                                               "{"
+                                                                               "   \"userId\": 7078106169577,"
+                                                                               "   \"status\": \"ACCEPTED\","
+                                                                               "   \"firstRequestedAt\": 1471046357339,"
+                                                                               "   \"updatedAt\": 1471046517684,"
+                                                                               "   \"requestCounter\": 1"
+                                                                               "}")
 
     user_connection = await connection_service.accept_connection(7078106169577)
 
@@ -130,23 +126,22 @@ async def test_accept_connection(connection_api, connection_service):
         session_token="session_token"
     )
 
-    assert user_connection.userId == 7078106169577
-    assert user_connection.firstRequestedAt == 1471046357339
+    assert user_connection.user_id == 7078106169577
+    assert user_connection.first_requested_at == 1471046357339
     assert user_connection.status == ConnectionStatus.ACCEPTED.value
 
 
 @pytest.mark.asyncio
 async def test_reject_connection(connection_api, connection_service):
     connection_api.v1_connection_reject_post = AsyncMock()
-    connection_api.v1_connection_reject_post.return_value = object_from_json(
-        "{"
-        "   \"userId\": 7215545059385,"
-        "   \"status\": \"REJECTED\","
-        "   \"firstRequestedAt\": 1471044955409,"
-        "   \"updatedAt\": 1471045390420,"
-        "   \"requestCounter\": 1"
-        "}"
-    )
+    connection_api.v1_connection_reject_post.return_value = deserialize_object(UserConnection,
+                                                                               "{"
+                                                                               "   \"userId\": 7215545059385,"
+                                                                               "   \"status\": \"REJECTED\","
+                                                                               "   \"firstRequestedAt\": 1471044955409,"
+                                                                               "   \"updatedAt\": 1471045390420,"
+                                                                               "   \"requestCounter\": 1"
+                                                                               "}")
 
     user_connection = await connection_service.reject_connection(7215545059385)
 
@@ -155,7 +150,7 @@ async def test_reject_connection(connection_api, connection_service):
         session_token="session_token"
     )
 
-    assert user_connection.userId == 7215545059385
+    assert user_connection.user_id == 7215545059385
     assert user_connection.status == ConnectionStatus.REJECTED.value
 
 
