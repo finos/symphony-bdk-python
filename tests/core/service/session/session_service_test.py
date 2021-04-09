9f051502a5bdccfe6d1e74a36c247a3aaa67a5a0
symphony-youness <76746033+symphony-youness@users.noreply.github.com>
2021-04-09 15:13:59 +0200

    PLAT-10555: Deserializing models in tests (#168)
    Update the tests to use the generated models instead of creating objects from a JSON file.

--- tests/core/service/session/session_service_test.py
+++ tests/core/service/session/session_service_test.py
@@ -5,7 +5,8 @@ import pytest
 from symphony.bdk.core.auth.auth_session import AuthSession
 from symphony.bdk.core.service.session.session_service import SessionService
 from symphony.bdk.gen.pod_api.session_api import SessionApi
-from tests.utils.resource_utils import object_from_json_relative_path
+from symphony.bdk.gen.pod_model.user_v2 import UserV2
+from tests.utils.resource_utils import get_deserialized_object_from_resource
 
 
 @pytest.fixture(name="auth_session")
@@ -33,9 +34,8 @@ def fixture_session_service(session_api, auth_session):
 @pytest.mark.asyncio
 async def test_get_session(session_api, session_service):
     session_api.v2_sessioninfo_get = AsyncMock()
-    session_api.v2_sessioninfo_get.return_value = object_from_json_relative_path("session/get_session.json")
+    session_api.v2_sessioninfo_get.return_value = get_deserialized_object_from_resource(UserV2,
+                                                                                        "session/get_session.json")
 
     session = await session_service.get_session()
     assert session.display_name == "Symphony Admin"
-
-
