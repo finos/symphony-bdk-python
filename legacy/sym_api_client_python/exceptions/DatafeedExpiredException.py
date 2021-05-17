63700561654d29c0f16960dd9d7337d7dd9dfb68
Elias Croze <elias.croze@symphony.com>
2021-05-17 17:05:55 +0200

    PLAT-10885: removed legacy folder from 2.0 branch as legacy SDK in master branch

--- legacy/sym_api_client_python/exceptions/DatafeedExpiredException.py
+++
@@ -1,5 +0,0 @@
-class DatafeedExpiredException(Exception):
-    """This exception represents the response below from the API that occurs periodically
-    when a datafeed has expired. It should be handled by creating a new datafeed and 
-    reading from there"""
-    pass
--- legacy/sym_api_client_python/exceptions/ServerErrorException.py
+++
@@ -1,2 +0,0 @@
-class ServerErrorException(Exception):
-    pass
