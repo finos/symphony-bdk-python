f318acc87d5d7dc2f67716c06f66f3288a557c9a
Elias Croze <elias.croze@symphony.com>
2021-05-17 16:17:09 +0200

    PLAT-10854: Added autoescape in jinja2 templates

--- sym_api_client_python/templates/elements_templates/render_templates.py
+++ sym_api_client_python/templates/elements_templates/render_templates.py
@@ -1,13 +1,13 @@
-import jinja2
 from jinja2 import Template
 
 form_template = {
-  "form_id": 'example-form-id'
-};
+    "form_id": 'example-form-id'
+}
+
 
 def render_form(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(form_template)
     return html
 
@@ -18,15 +18,16 @@ button_template = {
     "text": "Submit"
 }
 
+
 def render_button(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(button_template)
     return html
 
 
 text_field_template = {
-    "name":"exmaple-text-field",
+    "name": "exmaple-text-field",
     "placeholder": "example-placeholder",
     "required": "true",
     "masked": "true",
@@ -34,98 +35,104 @@ text_field_template = {
     "maxlength": 128
 }
 
+
 def render_text_field(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(text_field_template)
     return html
 
 
 text_area_template = {
-    "name":"exmaple-text-area",
+    "name": "exmaple-text-area",
     "placeholder": "example-placeholder",
     "required": "true",
 }
 
+
 def render_text_area(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(text_area_template)
     return html
 
 
 checkbox_template = {
-"name":"example-name",
-"value":"example-value",
-"checked": "false",
-"text":"Red"
+    "name": "example-name",
+    "value": "example-value",
+    "checked": "false",
+    "text": "Red"
 }
+
 
 def render_checkbox(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(checkbox_template)
     return html
 
 
 radio_button_template = {
-"name":"example-name",
-"value":"example-value",
-"checked": "false",
-"text":"Red"
+    "name": "example-name",
+    "value": "example-value",
+    "checked": "false",
+    "text": "Red"
 }
+
 
 def render_radio_button(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(radio_button_template)
     return html
 
 
 dropdown_menu_template = {
-"name":"dropdown-name",
-"required": "true",
-"options": [{"value":"value1", "selected":"true", "text":"First Option"},
-            {"value":"value2", "selected":"false", "text":"Second Option"},
-            {"value":"value3", "selected":"true", "text":"Third Option"} ]
+    "name": "dropdown-name",
+    "required": "true",
+    "options": [{"value": "value1", "selected": "true", "text": "First Option"},
+                {"value": "value2", "selected": "false", "text": "Second Option"},
+                {"value": "value3", "selected": "true", "text": "Third Option"}]
 
 }
 
+
 def render_dropdown_menu(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(dropdown_menu_template)
     return html
 
 
 person_selector_template = {
-"name":"person-selector-name",
-"placeholder":"example-placeholder",
-"required":"true"
+    "name": "person-selector-name",
+    "placeholder": "example-placeholder",
+    "required": "true"
 }
+
 
 def render_person_selector(path_to_html_form):
     with open(path_to_html_form) as file:
-        template = Template(file.read())
+        template = Template(file.read(), autoescape=True)
     html = template.render(person_selector_template)
     return html
 
 
 table_select_template = {
-"select":{
-    "position":"left",
-    "type":"button"
+    "select": {
+        "position": "left",
+        "type": "button"
     },
-"header_list": ["H1", "H2", "H3"],
-"body": [["A1", "B1", "C1"],
-         ["A2", "B2", "C2"],
-         ["A3", "B3", "C3"]],
-"footer_list": ["F1", "F2", "F3"]
+    "header_list": ["H1", "H2", "H3"],
+    "body": [["A1", "B1", "C1"],
+             ["A2", "B2", "C2"],
+             ["A3", "B3", "C3"]],
+    "footer_list": ["F1", "F2", "F3"]
 }
+
 
 def render_table_select(path_to_html_form):
     with open(path_to_html_form) as file:
         template = Template(file.read(), trim_blocks=True, lstrip_blocks=True)
     html = template.render(table_select_template)
     return html
-
