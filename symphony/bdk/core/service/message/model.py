symphony-elias/symphony-api-client-python

PLAT-10829: Added Message class to ease message sending

symphony/bdk/core/service/message/model.py
@@ -1,0 +1,103 @@
+from typing import List, BinaryIO, Tuple, Union
+
+from symphony.bdk.core.service.exception import MessageCreationError
+
+MESSAGE_ML_END_TAG = "</messageML>"
+MESSAGE_ML_START_TAG = "<messageML>"
+
+
+class Message:
+    """Class holding message information, used in
+    :py:class:`~symphony.bdk.core.service.message.message_service.MessageService` to send a message.
+    To know more about the format, see
+    `Create Message <https://developers.symphony.com/restapi/reference#create-message-v4>`_.
+
+    """
+
+    def __init__(self, content: str, data: str = "",
+                 attachments: List[Union[BinaryIO, Tuple[BinaryIO, BinaryIO]]] = None, version: str = ""):
+        """Builds a message.
+
+        :param content: the MessageML content to be sent. This is mandatory
+          If there is no <messageML> tags, they will be added to the content.
+        :param data: JSON string representing the objects contained in the message.
+        :param attachments: list of attachments or list of (attachment, previews).
+          These must be files opened in binary mode.
+          Previews are optional but if present, all attachments must have a preview.
+        :param version: Optional message version in the format "major.minor".
+          If empty, defaults to the latest supported version.
+        :raise MessageCreationError: if content is None,
+          or if number of previews different than the number of attachments (in case there is at least one preview).
+        """
+        self._content = self._get_content(content)
+        self._data = data
+        self._version = version
+        self._attachments, self._previews = self._get_attachments_and_previews(attachments)
+
+    @property
+    def content(self) -> str:
+        """Message content
+
+        :return: the messageML content
+        """
+        return self._content
+
+    @property
+    def data(self) -> str:
+        """Message data
+
+        :return: the data as a JSON string
+        """
+        return self._data
+
+    @property
+    def version(self) -> str:
+        """Message format version.
+
+        :return: the message format version
+        """
+        return self._version
+
+    @property
+    def attachments(self) -> List[BinaryIO]:
+        """List of attachments
+
+        :return: the list of attachments
+        """
+        return self._attachments
+
+    @property
+    def previews(self) -> List[BinaryIO]:
+        """List of previews
+
+        :return: the list of previews
+        """
+        return self._previews
+
+    @staticmethod
+    def _get_content(content):
+        if content is None:
+            raise MessageCreationError("Message content is mandatory")
+        if not content.startswith(MESSAGE_ML_START_TAG) and not content.endswith(MESSAGE_ML_END_TAG):
+            return MESSAGE_ML_START_TAG + content + MESSAGE_ML_END_TAG
+        return content
+
+    @staticmethod
+    def _get_attachments_and_previews(attachments_previews):
+        if attachments_previews is None:
+            return [], []
+
+        attachments = []
+        previews = []
+        for item in attachments_previews:
+            if isinstance(item, tuple) and len(item) >= 1:
+                attachments.append(item[0])
+                if len(item) >= 2:
+                    previews.append(item[1])
+            else:
+                attachments.append(item)
+
+        if previews and len(previews) != len(attachments):
+            raise MessageCreationError("Message should contain either no preview or as many previews as attachments")
+
+        return attachments, previews

tests/core/service/message/message_service_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from symphony.bdk.core.auth.auth_session import AuthSession
 from symphony.bdk.core.service.message.message_service import MessageService
+from symphony.bdk.core.service.message.model import Message
 from symphony.bdk.core.service.message.multi_attachments_messages_api import MultiAttachmentsMessagesApi
 from symphony.bdk.gen.agent_api.attachments_api import AttachmentsApi
 from symphony.bdk.gen.agent_model.v4_import_response_list import V4ImportResponseList
@@ -80,6 +81,35 @@ async def test_send_message(mocked_api_client, message_service):
 
 
 @pytest.mark.asyncio
+async def test_send_simple_message(message_service):
+    message_service.send_message = AsyncMock(
+        return_value=get_deserialized_object_from_resource(V4Message, "message_response/message.json"))
+    message = "<messageML>Hello</messageML>"
+    stream_id = "stream_id"
+
+    await message_service.send_simple_message(stream_id, message)
+
+    message_service.send_message.assert_called_once_with(stream_id, message, "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_send_complex_message(message_service):
+    message_service.send_message = AsyncMock(
+        return_value=get_deserialized_object_from_resource(V4Message, "message_response/message.json"))
+    stream_id = "stream_id"
+    content = "<messageML>Hello</messageML>"
+    data = """{"some": "data"}"""
+    version = "2.0"
+    attachment = "attachment"
+    preview = "preview"
+    message = Message(content=content, data=data, attachments=[(attachment, preview)], version=version)
+
+    await message_service.send_complex_message(stream_id, message)
+
+    message_service.send_message.assert_called_once_with(stream_id, content, data, version, [attachment], [preview])
+
+
+@pytest.mark.asyncio
 async def test_blast_message(mocked_api_client, message_service):
     mocked_api_client.call_api.return_value = \
         get_deserialized_object_from_resource(V4MessageBlastResponse, "message_response/blast_message.json")
@@ -88,6 +118,37 @@ async def test_blast_message(mocked_api_client, message_service):
     assert len(blast_message.messages) == 2
     assert blast_message.messages[0].message_id == "-AANBHKtUC-2q6_0WSjBGX___pHnSfBKdA-1"
     assert blast_message.messages[1].message_id == "-AANBHKtUC-2q6_0WSjBGX___pHnSfBKdA-2"
+
+
+@pytest.mark.asyncio
+async def test_blast_simple_message(message_service):
+    blast_message_response = get_deserialized_object_from_resource(V4MessageBlastResponse,
+                                                                   "message_response/blast_message.json")
+    message_service.blast_message = AsyncMock(return_value=blast_message_response)
+    stream_ids = ["sid1", "sid2"]
+    message = "<messageML>Hello</messageML>"
+
+    await message_service.blast_simple_message(stream_ids, message)
+
+    message_service.blast_message.assert_called_once_with(stream_ids, message, "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_blast_complex_message(message_service):
+    blast_message_response = get_deserialized_object_from_resource(V4MessageBlastResponse,
+                                                                   "message_response/blast_message.json")
+    message_service.blast_message = AsyncMock(return_value=blast_message_response)
+    stream_ids = ["sid1", "sid2"]
+    content = "<messageML>Hello</messageML>"
+    data = """{"some": "data"}"""
+    version = "2.0"
+    attachment = "attachment"
+    preview = "preview"
+    message = Message(content=content, data=data, attachments=[(attachment, preview)], version=version)
+
+    await message_service.blast_complex_message(stream_ids, message)
+
+    message_service.blast_message.assert_called_once_with(stream_ids, content, data, version, [attachment], [preview])
 
 
 @pytest.mark.asyncio

finos/symphony-bdk-python

symphony/bdk/core/service/message/model.py
@@ -1,0 +1,103 @@
+from io import IOBase
+from typing import List, Tuple, Union
+
+from symphony.bdk.core.service.exception import MessageCreationError
+
+MESSAGE_ML_END_TAG = "</messageML>"
+MESSAGE_ML_START_TAG = "<messageML>"
+
+
+class Message:
+    """Class holding message information, used in
+    :py:class:`~symphony.bdk.core.service.message.message_service.MessageService` to send a message.
+    To know more about the format, see
+    `Create Message <https://developers.symphony.com/restapi/reference#create-message-v4>`_.
+    """
+
+    def __init__(self, content: str, data: str = "",
+                 attachments: List[Union[IOBase, Tuple[IOBase, IOBase]]] = None, version: str = ""):
+        """Builds a message.
+
+        :param content: the MessageML content to be sent. This is mandatory
+          If there is no <messageML> tags, they will be added to the content.
+        :param data: JSON string representing the objects contained in the message.
+        :param attachments: list of attachments or list of (attachment, previews).
+          These must be opened files either in binary or text mode.
+          Previews are optional but if present, all attachments must have a preview.
+        :param version: Optional message version in the format "major.minor".
+          If empty, defaults to the latest supported version.
+        :raise MessageCreationError: if content is None,
+          or if number of previews different than the number of attachments (in case there is at least one preview).
+        """
+        self._content = self._get_content(content)
+        self._data = data
+        self._version = version
+        self._attachments, self._previews = self._get_attachments_and_previews(attachments)
+
+    @property
+    def content(self) -> str:
+        """Message content
+
+        :return: the messageML content
+        """
+        return self._content
+
+    @property
+    def data(self) -> str:
+        """Message data
+
+        :return: the data as a JSON string
+        """
+        return self._data
+
+    @property
+    def version(self) -> str:
+        """Message format version.
+
+        :return: the message format version
+        """
+        return self._version
+
+    @property
+    def attachments(self) -> List[IOBase]:
+        """List of attachments
+
+        :return: the list of attachments
+        """
+        return self._attachments
+
+    @property
+    def previews(self) -> List[IOBase]:
+        """List of previews
+
+        :return: the list of previews
+        """
+        return self._previews
+
+    @staticmethod
+    def _get_content(content):
+        if content is None:
+            raise MessageCreationError("Message content is mandatory")
+        if not content.startswith(MESSAGE_ML_START_TAG) and not content.endswith(MESSAGE_ML_END_TAG):
+            return MESSAGE_ML_START_TAG + content + MESSAGE_ML_END_TAG
+        return content
+
+    @staticmethod
+    def _get_attachments_and_previews(attachments_previews):
+        if attachments_previews is None:
+            return [], []
+
+        attachments = []
+        previews = []
+        for item in attachments_previews:
+            if isinstance(item, tuple) and len(item) >= 1:
+                attachments.append(item[0])
+                if len(item) >= 2:
+                    previews.append(item[1])
+            else:
+                attachments.append(item)
+
+        if previews and len(previews) != len(attachments):
+            raise MessageCreationError("Message should contain either no preview or as many previews as attachments")
+
+        return attachments, previews

tests/core/service/message/message_service_test.py
@@ -4,6 +4,7 @@ import pytest
 
 from symphony.bdk.core.auth.auth_session import AuthSession
 from symphony.bdk.core.service.message.message_service import MessageService
+from symphony.bdk.core.service.message.model import Message
 from symphony.bdk.core.service.message.multi_attachments_messages_api import MultiAttachmentsMessagesApi
 from symphony.bdk.gen.agent_api.attachments_api import AttachmentsApi
 from symphony.bdk.gen.agent_model.v4_import_response_list import V4ImportResponseList
@@ -80,6 +81,46 @@ async def test_send_message(mocked_api_client, message_service):
 
 
 @pytest.mark.asyncio
+async def test_send_simple_message(message_service):
+    message_service._send_message = AsyncMock(
+        return_value=get_deserialized_object_from_resource(V4Message, "message_response/message.json"))
+    message = "<messageML>Hello</messageML>"
+    stream_id = "stream_id"
+
+    await message_service.send_message(stream_id, message)
+
+    message_service._send_message.assert_called_once_with(stream_id, message, "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_send_simple_message_without_mml_tags(message_service):
+    message_service._send_message = AsyncMock(
+        return_value=get_deserialized_object_from_resource(V4Message, "message_response/message.json"))
+    stream_id = "stream_id"
+
+    await message_service.send_message(stream_id, "Hello")
+
+    message_service._send_message.assert_called_once_with(stream_id, "<messageML>Hello</messageML>", "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_send_complex_message(message_service):
+    message_service._send_message = AsyncMock(
+        return_value=get_deserialized_object_from_resource(V4Message, "message_response/message.json"))
+    stream_id = "stream_id"
+    content = "<messageML>Hello</messageML>"
+    data = """{"some": "data"}"""
+    version = "2.0"
+    attachment = "attachment"
+    preview = "preview"
+    message = Message(content=content, data=data, attachments=[(attachment, preview)], version=version)
+
+    await message_service.send_message(stream_id, message)
+
+    message_service._send_message.assert_called_once_with(stream_id, content, data, version, [attachment], [preview])
+
+
+@pytest.mark.asyncio
 async def test_blast_message(mocked_api_client, message_service):
     mocked_api_client.call_api.return_value = \
         get_deserialized_object_from_resource(V4MessageBlastResponse, "message_response/blast_message.json")
@@ -88,6 +129,48 @@ async def test_blast_message(mocked_api_client, message_service):
     assert len(blast_message.messages) == 2
     assert blast_message.messages[0].message_id == "-AANBHKtUC-2q6_0WSjBGX___pHnSfBKdA-1"
     assert blast_message.messages[1].message_id == "-AANBHKtUC-2q6_0WSjBGX___pHnSfBKdA-2"
+
+
+@pytest.mark.asyncio
+async def test_blast_simple_message(message_service):
+    blast_message_response = get_deserialized_object_from_resource(V4MessageBlastResponse,
+                                                                   "message_response/blast_message.json")
+    message_service._blast_message = AsyncMock(return_value=blast_message_response)
+    stream_ids = ["sid1", "sid2"]
+    message = "<messageML>Hello</messageML>"
+
+    await message_service.blast_message(stream_ids, message)
+
+    message_service._blast_message.assert_called_once_with(stream_ids, message, "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_blast_simple_message_without_mml_tags(message_service):
+    blast_message_response = get_deserialized_object_from_resource(V4MessageBlastResponse,
+                                                                   "message_response/blast_message.json")
+    message_service._blast_message = AsyncMock(return_value=blast_message_response)
+    stream_ids = ["sid1", "sid2"]
+    await message_service.blast_message(stream_ids, "Hello")
+
+    message_service._blast_message.assert_called_once_with(stream_ids, "<messageML>Hello</messageML>", "", "", [], [])
+
+
+@pytest.mark.asyncio
+async def test_blast_complex_message(message_service):
+    blast_message_response = get_deserialized_object_from_resource(V4MessageBlastResponse,
+                                                                   "message_response/blast_message.json")
+    message_service._blast_message = AsyncMock(return_value=blast_message_response)
+    stream_ids = ["sid1", "sid2"]
+    content = "<messageML>Hello</messageML>"
+    data = """{"some": "data"}"""
+    version = "2.0"
+    attachment = "attachment"
+    preview = "preview"
+    message = Message(content=content, data=data, attachments=[(attachment, preview)], version=version)
+
+    await message_service.blast_message(stream_ids, message)
+
+    message_service._blast_message.assert_called_once_with(stream_ids, content, data, version, [attachment], [preview])
 
 
 @pytest.mark.asyncio