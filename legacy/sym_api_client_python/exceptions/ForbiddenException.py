5a48f8068945184911bd33fcce67e9c3b5aed6d1
Elias Croze <66254040+symphony-elias@users.noreply.github.com>
2021-05-18 09:24:48 +0200

    PLAT-10885: removed legacy folder from 2.0 branch (#190)

--- legacy/sym_api_client_python/exceptions/ForbiddenException.py
+++
@@ -1,2 +0,0 @@
-class ForbiddenException(Exception):
-    pass
--- legacy/tests/util/resource_util.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def get_resource_filepath(path_relative_to_resources):
-	filepath = os.path.join(os.path.dirname(__file__), '../resources/', path_relative_to_resources)
-	return os.path.normpath(filepath)
