cce7bafb664d7f6a42cc222e9357794364708a5b
Elias Croze <elias.croze@symphony.com>
2022-02-16 15:05:18 +0100

    Extension API PoC

---
+++ examples/services/extension_example.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging.config
+from pathlib import Path
+
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.config.loader import BdkConfigLoader
+from symphony.bdk.core.config.model.bdk_config import BdkConfig
+from symphony.bdk.core.symphony_bdk import SymphonyBdk
+
+
+class MyExtension:
+    def __init__(self):
+        self._bot_session = None
+        self._config = None
+
+    def set_auth_session(self, auth_session: AuthSession):
+        logger.debug("Registering auth session")
+        self._bot_session = auth_session
+
+    def set_configuration(self, bdk_config: BdkConfig):
+        logger.debug("Registering config: " + bdk_config.host)
+        self._config = bdk_config
+
+    def get_service(self):
+        return self
+
+    async def get_session_token(self):
+        return await self._bot_session.session_token
+
+
+async def run():
+    async with SymphonyBdk(BdkConfigLoader.load_from_symphony_dir("config.yaml")) as bdk:
+        bdk.extensions().register(MyExtension)
+        service = bdk.extensions().get_service(MyExtension)
+        logger.debug("Session token: " + await service.get_session_token())
+
+
+logging.config.fileConfig(Path(__file__).parent.parent / "logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+asyncio.run(run())
---
+++ symphony/bdk/core/extension.py
@@ -0,0 +1,54 @@
+import logging
+from abc import ABC, abstractmethod
+
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.config.model.bdk_config import BdkConfig
+
+logger = logging.getLogger(__name__)
+
+
+class BdkConfigAware(ABC):
+    @abstractmethod
+    def set_configuration(self, bdk_config: BdkConfig):
+        pass
+
+
+class BdkAuthenticationAware(ABC):
+    @abstractmethod
+    def set_auth_session(self, auth_session: AuthSession):
+        pass
+
+
+class ExtensionService:
+    def __init__(self, bot_session: AuthSession, bdk_config: BdkConfig):
+        self._bot_session = bot_session
+        self._bdk_config = bdk_config
+        self._extensions = {}
+        # what do we do if no bot configured (OBO-only usecase)?
+
+    def register(self, extension_type: type):
+        if extension_type in self._extensions:
+            raise ValueError(f"Extension {extension_type} already registered")
+        extension = extension_type.__new__(extension_type)
+        self._extensions[extension_type] = extension
+
+        try:
+            extension.set_configuration(self._bdk_config)
+        except AttributeError:
+            logging.debug("Extension is not a configuration aware")
+        except TypeError:
+            logging.warning("set_configuration method must have a single positional argument")
+
+        try:
+            extension.set_auth_session(self._bot_session)
+        except AttributeError:
+            logging.debug("Extension is not a authentication aware")
+        except TypeError:
+            logging.warning("set_auth_session method must have a single positional argument")
+
+    def get_service(self, extension_type):
+        extension = self._extensions[extension_type]
+        if extension is None:
+            raise ValueError(f"Extension {extension_type} not defined")
+
+        return extension.get_service()  # will raise an AttributeError or TypeError if get_service() not defined
