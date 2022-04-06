12ed27346f071699bc5b901e38cb29405afad944
symphony-soufiane <soufiane.aourinmouche@symphony.com>
2022-04-06 11:17:28 +0200

    Updated generated code to fix pagination cursors as optional.

--- api_client_generation/generate.sh
+++ api_client_generation/generate.sh
@@ -4,7 +4,7 @@ code_gen_dir=`pwd`
 project_root=$code_gen_dir/..
 echo $code_gen_dir
 
-commit_hash=16a902e2d0f8f64c680b5d799f871a3fc4884890
+commit_hash=2957cb3334a6eff0355f464aa71bbdfb92ae20d2
 api_spec_base_url=https://raw.githubusercontent.com/symphonyoss/symphony-api-spec/${commit_hash}
 echo $api_spec_base_url
 
